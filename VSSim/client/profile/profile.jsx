6abcf82e3d8b21a132e6bf7379de681a67eebea1
Erik Tillberg <eatillberg@gmail.com>
2016-10-31 18:29:38 -0400

    super fun side nav (like super fun)

--- VSSim/client/profile/profile.jsx
+++ VSSim/client/profile/profile.jsx
@@ -4,12 +4,50 @@ import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
 export default class Profile extends TrackerReact(React.Component){
 
+  openNav(){
+    document.getElementById("stockPickerId").style.width = "250px";
+    document.getElementById("profileID").style.marginLeft = "250px";
+  }
+
+  closeNav(){
+    document.getElementById("stockPickerId").style.width = "0";
+    document.getElementById("profileID").style.marginLeft = "0";
+  }
+
   render(){
     return (
-      <div className = "profile">
-        <h1>Profile</h1>
+      <div>
+        <div id = "profileID" className = "profile">
+          <h1>Profile</h1>
+          <button onClick = {this.openNav}>Choose Stock</button>
+          <p>
+            some content about stuff, i dunno.
+          </p>
+        </div>
+
+        <div id = "stockPickerId" className = "stockPicker">
+          <h2>Your Stocks</h2>
+          <a href = "javascript:void(0)" className = "closebtn" onClick = {this.closeNav}>&times;</a>
+          <ul id = "stockList">
+            <li>GOOG</li>
+            <li>TSLA</li>
+            <li>MSFT</li>
+            <li>DOWJ</li>
+          </ul>
+          <h2>Tracked Stocks</h2>
+            <ul id = "stockList">
+              <li>GOOG</li>
+              <li>TSLA</li>
+              <li>MSFT</li>
+              <li>DOWJ</li>
+              <li>TSLA</li>
+              <li>MSFT</li>
+              <li>DOWJ</li>
+            </ul>
+        </div>
 
       </div>
+
     )
   }
 
