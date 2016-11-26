9b441a07aea24ecae512d4eac9d6585b3242a6c6
ErikTillberg <eatillberg@gmail.com>
2016-11-26 18:28:16 -0500

    Profile page additions, table, info

--- VSSim/client/profile/profile.jsx
+++ VSSim/client/profile/profile.jsx
@@ -4,12 +4,6 @@ import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
 export default class Profile extends TrackerReact(React.Component){
 
-  componentWillUpdate(){
-    if (this.state.user != Meteor.user()){
-        this.setState({user: Meteor.user()})
-    }
-  }
-
   constructor(){
     super();
     this.state = {
@@ -44,6 +38,26 @@ export default class Profile extends TrackerReact(React.Component){
 
   }
 
+  calculateStockValue(){
+    //This method calculates the networth of the logged in user
+    var owned = this.state.user.stocksOwned;
+    var stockVal = 0;
+
+    for (var key in owned){
+      stockVal += owned[key].count*owned[key].lastCost;
+    }
+
+    return stockVal;
+
+  }
+
+  calculateNetworth(){
+    var stockVal = this.calculateStockValue();
+
+    return (stockVal + this.state.user.cashOnHand);
+
+  }
+
   openNav(){
     document.getElementById("closebtnId").style.marginLeft = "210px";
     document.getElementById("stockPickerId").style.width = "250px";
@@ -56,10 +70,20 @@ export default class Profile extends TrackerReact(React.Component){
     document.getElementById("profileID").style.marginLeft = "0";
   }
 
+  stocks(){
+    var owned = this.state.user.stocksOwned;
+
+    var ret = [];
+
+    for (var key in owned){
+      ret.push({symbol: key, count: owned[key].count, cost: owned[key].lastCost});
+    }
+    return ret;
+  }
+
   render(){
 
     return (
-
       <div>
 
         <ul className = "tab">
@@ -69,8 +93,37 @@ export default class Profile extends TrackerReact(React.Component){
         </ul>
 
 
-        <div id = "profileHome" className = "tabcontent">
-          <h2>{this.state.user? this.state.user.emails[0].address : 'Loading'} Profile</h2>
+        <div id = "profileHome" className = "tabcontent container-fluid">
+          <div className = "row">
+            <h2 className = "profileTitle">{this.state.user? this.state.user.emails[0].address : 'Loading'} Profile</h2>
+          </div>
+          <div className = "row profileInfo">
+            <div className = "col-xs-2"><h4>User Information</h4></div>
+            <div className = "col-xs-8">
+              <h4>Cash: {this.state.user? this.state.user.cashOnHand.toFixed(2) : 'Loading'}</h4>
+              <h4>Stock Value: {this.state.user? this.calculateStockValue().toFixed(2) : 'Loading'}</h4>
+              <h4>Networth: {this.state.user? this.calculateNetworth().toFixed(2) : 'Loading'}</h4>
+            </div>
+            <div className = "col-xs-2"></div>
+          </div>
+          <div className = "row">
+            <div className = "col-xs-2"><h4>Stock Information</h4></div>
+            <div className = "col-xs-8">
+              <table className = "table table-striped">
+                <thead><tr><th>Symbol</th><th>Count</th><th>Cost</th></tr></thead>
+                <tbody>
+                {this.state.user?
+                    this.stocks().map((stock) => {
+                      return (<tr><td>{stock.symbol}</td><td>{stock.count}</td><td>{stock.cost}</td></tr>)
+                    }
+                ):<tr><td>Loading</td><td>Loading</td><td>Loading</td></tr>}
+                </tbody>
+              </table>
+            </div>
+            <div className = "col-xs-2"></div>
+          </div>
+
+
         </div>
 
         <div id = "stockPickerId" className = "stockPicker">
